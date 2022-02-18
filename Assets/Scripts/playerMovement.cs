a673f336de126d8b7702820b77226cf974ee6711
Bailym <bm39@hotmail.co.uk>
2022-02-18 10:09:40 +0000

    add player movement

---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody2D playerBody;
+    public float maxVelocity;
+    void Start()
+    {
+        playerBody = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //basic axis based movement 
+        float inputDirectionHorizontal = Input.GetAxis("Horizontal");
+        float inputDirectionVertical = Input.GetAxis("Vertical");
+
+        inputDirectionHorizontal = Mathf.Clamp(inputDirectionHorizontal, -maxVelocity, maxVelocity);
+        inputDirectionVertical = Mathf.Clamp(inputDirectionVertical, -maxVelocity, maxVelocity);
+
+        Vector2 moveDirection = new Vector2(inputDirectionHorizontal, inputDirectionVertical);
+
+        playerBody.velocity = moveDirection;
+    }
+}
