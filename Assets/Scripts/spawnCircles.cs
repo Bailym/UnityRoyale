7593f22f12be36632cf7ed2a82d67cce06836ca3
Bailym <bm39@hotmail.co.uk>
2022-02-20 18:29:27 +0000

    circles randomly spawn during game

---
+++ Assets/Scripts/spawnCircles.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnCircles : MonoBehaviour
+{
+    private Game game;
+    public GameObject[] circleList;
+    public float spawnRate = 5f;
+    public Camera mainCam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        game = FindObjectOfType<Game>();
+        InvokeRepeating("SpawnCircle", 1f, spawnRate);  //1s delay, repeat every 1s
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        
+    }
+
+    void SpawnCircle()
+    {
+        int randomNum = Random.Range(0, circleList.Length);
+        GameObject prefab = circleList[randomNum];
+        Vector2 randomPositionOnScreen = Camera.main.ViewportToWorldPoint(new Vector2(Random.value, Random.value));
+        Instantiate(prefab, randomPositionOnScreen, Quaternion.identity);
+    }
+}
